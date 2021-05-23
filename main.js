36a6e97f3e404ef0045ce0c8712686e005e8dec0
Nispaksh <80155055+Nispaksh@users.noreply.github.com>
2021-05-23 13:53:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+n_o_p = [];
+function Submit(){
+    display_guest_array=[]
+    var guestname = document.getElementById("Guest1").value;
+    n_o_p.push(guestname);
+    console.log(n_o_p);
+    n_o_p.sort();
+    for(var j =1; j<50; j++){
+        if(j=j>50){
+            window.prompt(Sorry !)
+        }
+    }
+}
+       
+       
